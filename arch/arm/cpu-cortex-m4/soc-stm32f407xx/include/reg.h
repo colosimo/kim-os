aedf7a000b470dc2645d6cdb8848f786f720fae7
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2019-11-27 14:32:50 +0100

    USB OTG FS registers defined for soc stm32f407
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

--- arch/arm/cpu-cortex-m4/soc-stm32f407xx/include/reg.h
+++ arch/arm/cpu-cortex-m4/soc-stm32f407xx/include/reg.h
@@ -90,4 +90,63 @@
 #define R_GPIOA_AFRL    reg32(0x40020020)
 #define R_GPIOA_AFRH    reg32(0x40020024)
 
+/* OTG_FS core global registers */
+#define R_OTG_FS_GOTGCTL   reg32(0x50000000)
+#define R_OTG_FS_GOTGINT   reg32(0x50000004)
+#define R_OTG_FS_GAHBCFG   reg32(0x50000008)
+#define R_OTG_FS_GUSBCFG   reg32(0x5000000C)
+#define R_OTG_FS_GRSTCTL   reg32(0x50000010)
+#define R_OTG_FS_GINTSTS   reg32(0x50000014)
+#define R_OTG_FS_GINTMSK   reg32(0x50000018)
+#define R_OTG_FS_GRXSTSR   reg32(0x5000001C)
+#define R_OTG_FS_GRXSTSP   reg32(0x50000020)
+#define R_OTG_FS_GRXFSIZ   reg32(0x50000024)
+#define R_OTG_FS_HNPTXFSIZ reg32(0x50000028)
+#define R_OTG_FS_DIEPTXF0  reg32(0x50000028)
+#define R_OTG_FS_HNPTXSTS  reg32(0x5000002C)
+#define R_OTG_FS_GCCFG     reg32(0x50000038)
+#define R_OTG_FS_CID       reg32(0x5000003C)
+#define R_OTG_FS_HPTXFSIZ  reg32(0x50000100)
+#define R_OTG_FS_DIEPTXF1  reg32(0x50000104)
+#define R_OTG_FS_DIEPTXF2  reg32(0x50000108)
+#define R_OTG_FS_DIEPTXF3  reg32(0x5000010C)
+
+/* OTG_FS host mode registers */
+#define R_OTG_FS_HCFG      reg32(0x50000400)
+#define R_OTG_FS_HFIR      reg32(0x50000404)
+#define R_OTG_FS_HFNUM     reg32(0x50000408)
+#define R_OTG_FS_HPTXSTS   reg32(0x50000410)
+#define R_OTG_FS_HAINT     reg32(0x50000414)
+#define R_OTG_FS_HAINTMSK  reg32(0x50000418)
+#define R_OTG_FS_HPRT      reg32(0x50000440)
+#define R_OTG_FS_HCCHAR(x)   reg32(0x50000500 + ((x) * 0x20))
+#define R_OTG_FS_HCINT(x)    reg32(0x50000508 + ((x) * 0x20))
+#define R_OTG_FS_HCINTMSK(x) reg32(0x5000050C + ((x) * 0x20))
+#define R_OTG_FS_HCTSIZ(x)   reg32(0x50000510 + ((x) * 0x20))
+
+/* OTG_FS device mode registers */
+#define R_OTG_FS_DCFG      reg32(0x50000800)
+#define R_OTG_FS_DCTL      reg32(0x50000804)
+#define R_OTG_FS_DSTS      reg32(0x50000808)
+#define R_OTG_FS_DIEPMSK   reg32(0x50000810)
+#define R_OTG_FS_DOEPMSK   reg32(0x50000814)
+#define R_OTG_FS_DAINT     reg32(0x50000818)
+#define R_OTG_FS_DAINTMSK  reg32(0x5000081C)
+#define R_OTG_FS_DVBUSDIS  reg32(0x50000828)
+#define R_OTG_FS_DVBUSPULSE   reg32(0x5000082C)
+#define R_OTG_FS_DIEPEMPMSK   reg32(0x50000834)
+#define R_OTG_FS_DIEPCTL(x)   reg32(0x50000900 + (x) * 0x20)
+#define R_OTG_FS_DIEPINT(x)   reg32(0x50000908 + (x) * 0x20)
+#define R_OTG_FS_DIEPTSIZ0(x) reg32(0x50000910 + (x) * 0x20)
+#define R_OTG_FS_DTXFSTS(x)   reg32(0x50000918 + (x) * 0x20)
+#define R_OTG_FS_DOEPCTL(x)   reg32(0x50000B00 + (x) * 0x20)
+#define R_OTG_FS_DOEPINT(x)   reg32(0x50000B08 + (x) * 0x20)
+#define R_OTG_FS_DOEPTSIZ0(x) reg32(0x50000B10 + (x) * 0x20)
+
+/* OTG_FS power and clock gating registers */
+#define R_OTG_FS_PCGCCTL   reg32(0x50000E00)
+
+/* DFIFO push/pop registers */
+#define R_OTG_FS_DFIFO(x)  reg32(0x50001000 + (x) * 0x1000)
+
 #endif /* _REG_H_ */
