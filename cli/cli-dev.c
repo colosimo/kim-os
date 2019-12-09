fae662fe5d13c98729dba9d451aa0c049c2a8a8d
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2019-12-09 14:44:42 +0100

    new CLI commands: r (read) and w (write)
    Two commands are here implemented for simple I/O interaction:
    - w: writes a sequence of bytes (given as hex numbers) to a device;
    - r: reads bytes from a device.
    
    Detailed usage is in helper description:
    
    r
            Read data from a device and dump it.
            Usage: r <devname> <maxbytes> (maxbytes<0 to flush all)
    w
            Write a hex sequence to a device (max 32 bytes).
            Usage: w <devname> <byte1> <byte2> ...
    
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

--- cli/cli-dev.c
+++ cli/cli-dev.c
@@ -18,6 +18,8 @@
 #include <kim-io-defs.h>
 #include <errcode.h>
 
+#define IO_BUF_LEN 32
+
 static int lsdev_cmd_cb(int argc, char *argv[], int fdout)
 {
 	k_fprintf(fdout, "maj\tmin\tname\n");
@@ -30,4 +32,74 @@ const struct cli_cmd_t attr_cli cli_lsdev = {
 	.cmd = lsdev_cmd_cb,
 	.name = "lsdev",
 	.descr = "List devices"
+};
+
+static int w_cmd_cb(int argc, char *argv[], int fdout)
+{
+	int fd;
+	int i;
+	u8 buf[IO_BUF_LEN];
+
+	fd = k_fd_byname(argv[1]);
+
+	if (fd < 0) {
+		k_fprintf(fdout, "dev not found: <%s>\n", argv[1]);
+		return -ERRINVAL;
+	}
+
+	if (argc - 2 > sizeof(buf)) {
+		k_fprintf(fdout, "%s: max " str(IO_BUF_LEN) " bytes allowed\n",
+			argv[0]);
+		return -ERRINVAL;
+	}
+
+	for (i = 0; i < argc - 2; i++)
+		buf[i] = atoi_hex(argv[i + 2]);
+
+	k_write(fd, buf, i);
+
+	return 0;
+}
+
+const struct cli_cmd_t attr_cli cli_w = {
+	.narg = 2,
+	.cmd = w_cmd_cb,
+	.name = "w",
+	.descr = "Write a hex sequence to a device (max " str(IO_BUF_LEN) " bytes).\r\n"
+	    "\tUsage: w <devname> <byte1> <byte2> ...",
+};
+
+static int r_cmd_cb(int argc, char *argv[], int fdout)
+{
+	int fd;
+	int i, n, rd;
+	u8 buf[IO_BUF_LEN];
+
+	fd = k_fd_byname(argv[1]);
+
+	if (fd < 0) {
+		k_fprintf(fdout, "dev not found: <%s>\n", argv[1]);
+		return -ERRINVAL;
+	}
+
+	n = atoi(argv[2]);
+
+	while (n) {
+		rd = k_read(fd, buf, min(IO_BUF_LEN, n));
+		if (rd <= 0)
+			break;
+		for (i = 0; i < rd; i++)
+			k_fprintf(fdout, "%02x ", buf[i]);
+		n-= rd;
+	}
+
+	return 0;
+}
+
+const struct cli_cmd_t attr_cli cli_r = {
+	.narg = 2,
+	.cmd = r_cmd_cb,
+	.name = "r",
+	.descr = "Read data from a device and dump it.\r\n"
+	    "\tUsage: r <devname> <maxbytes> (maxbytes < 0 to flush all)",
 };
