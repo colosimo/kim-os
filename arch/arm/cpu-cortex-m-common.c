9a11b3dc8d7010ba2fa09db40e9bbee878182acb
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2020-08-05 10:38:59 +0200

    verbose hard fault oops message moved to cortex-m4 only
    The assembly code performing oops messaging is valid on
    cortex-m4 only.
    
    Note: this commit fixes previous commit e01f207.
    
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

--- arch/arm/cpu-cortex-m-common.c
+++ arch/arm/cpu-cortex-m-common.c
@@ -15,57 +15,15 @@
 
 extern void init(void);
 
-void dump_stack(uint32_t *stack)
+void attr_weak isr_hf(void)
 {
-	volatile uint32_t r0;
-	volatile uint32_t r1;
-	volatile uint32_t r2;
-	volatile uint32_t r3;
-	volatile uint32_t r12;
-	volatile uint32_t lr; /* link register */
-	volatile uint32_t pc; /* program counter */
-	volatile uint32_t psr;/* program status register */
-
-	r0 = stack[0];
-	r1 = stack[1];
-	r2 = stack[2];
-	r3 = stack[3];
-	r12 = stack[4];
-	lr = stack[5];
-	pc = stack[6];
-	psr = stack[7];
-
-	crt("oops\n");
-	crt("r0=%08x\n", (uint)r0);
-	crt("r1=%08x\n", (uint)r1);
-	crt("r2=%08x\n", (uint)r2);
-	crt("r3=%08x\n", (uint)r3);
-	crt("r12=%08x\n", (uint)r12);
-	crt("lr=%08x\n", (uint)lr);
-	crt("pc=%08x\n", (uint)pc);
-	crt("psr=%08x\n", (uint)psr);
-
-	while (1);
-}
-
-void attr_weak attr_naked isr_hf(void)
-{
-	__asm volatile (
-		"tst lr, #4\n"
-		"ite eq\n"
-		"mrseq r0, msp\n"
-		"mrsne r0, psp\n"
-		"ldr r1, [r0, #24]\n"
-		"ldr r2, addr_const\n"
-		"bx r2\n"
-		"addr_const: .word dump_stack\n"
-	);
+	crt("Hard Fault\n");
+	while(1);
 }
 
 void attr_weak isr_none(void)
 {
-	crt("Panic: unhandled ISR\n");
-	isr_hf();
+	crt("Unhandled ISR\n");
 	while (1);
 }
 
--- arch/arm/cpu-cortex-m4/cpu.c
+++ arch/arm/cpu-cortex-m4/cpu.c
@@ -13,14 +13,6 @@
 #include <reg.h>
 #include <log.h>
 
-void isr_none(void)
-{
-	u32 icsr = rd32(R_SCB_ICSR);
-	crt("Unhandled ICSR %08x, (IRQ %d)\n", (uint)icsr,
-	    ((int)icsr & 0x1ff) - 16);
-	while(1);
-}
-
 static const void *attr_isrv_sys _isrv_sys[] = {
 	/* Cortex-M4 system interrupts */
 	STACK_TOP,	/* Stack top */
@@ -40,3 +32,59 @@ static const void *attr_isrv_sys _isrv_sys[] = {
 	isr_none,	/* PendSV */
 	isr_systick,	/* SysTick */
 };
+
+void dump_stack(uint32_t *stack)
+{
+	volatile uint32_t r0;
+	volatile uint32_t r1;
+	volatile uint32_t r2;
+	volatile uint32_t r3;
+	volatile uint32_t r12;
+	volatile uint32_t lr; /* link register */
+	volatile uint32_t pc; /* program counter */
+	volatile uint32_t psr;/* program status register */
+
+	r0 = stack[0];
+	r1 = stack[1];
+	r2 = stack[2];
+	r3 = stack[3];
+	r12 = stack[4];
+	lr = stack[5];
+	pc = stack[6];
+	psr = stack[7];
+
+	crt("oops\n");
+	crt("r0=%08x\n", (uint)r0);
+	crt("r1=%08x\n", (uint)r1);
+	crt("r2=%08x\n", (uint)r2);
+	crt("r3=%08x\n", (uint)r3);
+	crt("r12=%08x\n", (uint)r12);
+	crt("lr=%08x\n", (uint)lr);
+	crt("pc=%08x\n", (uint)pc);
+	crt("psr=%08x\n", (uint)psr);
+
+	while (1);
+}
+
+void attr_naked isr_hf(void)
+{
+	__asm volatile (
+		"tst lr, #4\n"
+		"ite eq\n"
+		"mrseq r0, msp\n"
+		"mrsne r0, psp\n"
+		"ldr r1, [r0, #24]\n"
+		"ldr r2, addr_const\n"
+		"bx r2\n"
+		"addr_const: .word dump_stack\n"
+	);
+}
+
+void isr_none(void)
+{
+	u32 icsr = rd32(R_SCB_ICSR);
+	crt("Unhandled ICSR %08x, (IRQ %d)\n", (uint)icsr,
+	    ((int)icsr & 0x1ff) - 16);
+	isr_hf();
+	while(1);
+}
